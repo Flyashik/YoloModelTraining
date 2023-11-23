bdbf7252ee21f379fa8614503978795d6cb6182e
Flyagin ID <flyagin00@mail.ru>
2023-11-23 21:07:20 +0300

    добавил main

--- train.py
+++ train.py
@@ -2,16 +2,16 @@ from ultralytics import YOLO
 import torch
 import shutil
 
-runs_folder = 'runs'
-shutil.rmtree(runs_folder, ignore_errors=True)
+if __name__ == '__main__':
+    runs_folder = 'runs'
+    shutil.rmtree(runs_folder, ignore_errors=True)
 
-device = '0' if torch.cuda.is_available() else 'cpu'
-if device == '0':
-    torch.cuda.set_device(0)
+    device = '0' if torch.cuda.is_available() else 'cpu'
+    if device == '0':
+        torch.cuda.set_device(0)
 
+    model = YOLO('yolov8n.pt', task='detect')
 
-model = YOLO('yolov8n.pt', task='detect')
+    results = model.train(data='data.yaml', epochs=10, mode='train', imgsz=640, name='train')
 
-results = model.train(data='data.yaml', epochs=10, mode='train', imgsz=640, name='train')
-
-print(results)
+    print(results)
